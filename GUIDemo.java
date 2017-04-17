060da7f0f7fcb9cc56e4827eac6df7619a2ea8b0
Bole <bolebj@l14354.cs.jmu.edu>
2017-04-17 14:10:53 -0400

    added new button to GUIDemo.

--- GUIDemo.java
+++ GUIDemo.java
@@ -1,6 +1,8 @@
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
+import java.awt.Color;
 
 import javax.swing.*;
 
@@ -15,13 +17,28 @@ public class GUIDemo extends JFrame
     private JPanel panel;
     private JButton biggerButton;
     private JButton smallerButton;
+	private JButton colorButton;
 
     /**
      * Set up the application.
      */
     public GUIDemo()
     {
-	// CONSTRUCTOR NEEDS TO BE FINISHED!
+	    setTitle("Bigger/Smaller");
+        setSize(200, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new JPanel();
+        biggerButton = new JButton("BIGGER");
+        smallerButton = new JButton("SMALLER");
+		colorButton = new JButton("Namer");
+        biggerButton.addActionListener(new ButtonHandler());
+        smallerButton.addActionListener(new ButtonHandler());
+		colorButton.addActionListener(new Namer());
+        add(panel);
+        panel.add(biggerButton);
+        panel.add(smallerButton);
+		panel.add(colorButton);
+        setVisible(true);
     }
 
     /**
@@ -49,6 +66,23 @@ public class GUIDemo extends JFrame
 
         }
     }
+
+    private class Namer implements ActionListener
+    {
+		public void actionPerformed(ActionEvent e)
+		{
+			Random gen = new Random();
+			if (gen.nextDouble() > .5)
+			{
+				setTitle("cool name");
+			}
+			else 
+			{
+				setTitle("awesome name");
+			}
+		}
+	}
+			
 
     /**
      * Start the app by creating a GUIDemo object.
